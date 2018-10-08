cedced2053abca5f7199ff6529f7e616c2d1af31
Christoffer Vorgaard Ashorn <201405541@post.au.dk>
2018-10-08 14:44:05 +0200

    removed EtaCiv cityhandling

--- src/main/java/hotciv/standard/SemiCivFactory.java
+++ src/main/java/hotciv/standard/SemiCivFactory.java
@@ -30,6 +30,6 @@ public class SemiCivFactory implements Factory {
 
     @Override
     public CityHandlingStrategy createCityHandlingStrategy(GameImpl game) {
-        return new CityHandlingStrategyEtaCiv(game);
+        return new CityHandlingStrategyAlphaCiv();
     }
 }
